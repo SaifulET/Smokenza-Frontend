87207212b3fc4caedb75bc8001bddc6e5f35a8fb
Saiful Islam <si912999@gmail.com>
2026-07-29 16:37:24 +0600

    Change baseURL for axios instance

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://ciger-backend-2.onrender.com",
+  // baseURL: "https://ciger-backend-2.onrender.com",
+  baseURL: "https://ciger-backend.vercel.app",
   // baseURL: "https://backend.smokenza.com",
   // baseURL:"http://localhost:5001",
   // optional
